b86bd9e99226ed1bd6f825f79125b2c28983c881
igor <igor@shapegames.com>
2023-02-27 11:13:11 +0100

    start setting up

--- app/src/main/java/com/grig/mylittlehelper/MainActivity.kt
+++ app/src/main/java/com/grig/mylittlehelper/MainActivity.kt
@@ -3,12 +3,14 @@ package com.grig.mylittlehelper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.grig.mylittlehelper.ui.theme.MyLittleHelperTheme
 
@@ -31,10 +33,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var items = 0
+    Column {
+        for (i in 0..5) {
+            Text(
+                text = "Hello $name $i!",
+                color = Color.Unspecified
+            )
+            items++
+        }
+        Text("Count: $items")
+    }
 }
 
 @Preview(showBackground = true)
+//@Preview(
+//    name = "Night mode",
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true
+//)
 @Composable
 fun DefaultPreview() {
     MyLittleHelperTheme {
---
+++ app/src/main/java/com/grig/mylittlehelper/MyAnimeListRepository.kt
@@ -0,0 +1,8 @@
+package com.grig.mylittlehelper
+
+import kotlinx.coroutines.flow.Flow
+
+class MyAnimeListRepository {
+
+//    val myAnileListFlow : Flow<String>
+}
