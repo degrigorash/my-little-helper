b81791484e579e00544e5561b8a712ba9e68df4c
igor <igor@shapegames.com>
2023-02-27 11:26:54 +0100

    bump some versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,13 +2,16 @@ object Versions {
     const val kotlin = "1.8.10"
     const val kotlinSerialization = "1.5.0"
     const val coreKtx = "1.9.0"
+
     const val androidPlugin = "7.4.0"
     const val lifecycleRuntimeKtx = "2.5.1"
+    const val material3 = "1.0.1"
+
     const val composeCompiler = "1.4.3"
     const val composeActivity = "1.6.1"
     const val composeViewModel = "2.5.1"
     const val compose = "1.3.3"
-    const val material3 = "1.0.0-alpha11"
+
     const val junit = "4.13.2"
     const val androidxJunit = "1.1.5"
     const val espressoCore = "3.5.1"
