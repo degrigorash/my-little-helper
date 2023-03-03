5ab778bdb9230816d8694071b5d33ac71e6b015b
ikozlenko <ikozlenko@toogoodtogo.com>
2023-03-03 09:28:01 +0100

    rebase1

--- app/src/main/java/com/grig/mylittlehelper/ui/theme/Color.kt
+++ app/src/main/java/com/grig/mylittlehelper/ui/theme/Color.kt
@@ -10,5 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Rebase1 = Color(0xFF7D5260)
+
 val Base1 = Color(0xFF7D5260)
 val Base2 = Color(0xFF7D5260)
