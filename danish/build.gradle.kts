1ecde275aa4859de9a182662adbb002f4922dd1c
ikozlenko <ikozlenko@toogoodtogo.com>
2025-09-25 12:54:35 +0200

    add danish module

---
+++ danish/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    kotlin("kapt")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlinx-serialization")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.grig.danish"
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.composeCompiler
+    }
+}
+
+dependencies {
+    implementation(Dependencies.kotlin)
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.kotlinxSerialization)
+    implementation(Dependencies.timber)
+    implementation(Dependencies.datastore)
+
+    implementation(Dependencies.kotlinxSerializationRetrofit)
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.okhttp3)
+
+    implementation(Dependencies.hilt)
+    kapt(Dependencies.hiltCompiler)
+
+    implementation(Dependencies.material)
+    implementation(Dependencies.composeActivity)
+    implementation(Dependencies.composeViewModel)
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeNavigation)
+    implementation(Dependencies.composeNavigationHilt)
+    implementation(Dependencies.composeUiToolingPreview)
+
+    implementation(Dependencies.coil)
+
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.androidxJunit)
+    androidTestImplementation(Dependencies.espressoCore)
+    androidTestImplementation(Dependencies.composeUiTest)
+
+    debugImplementation(Dependencies.composeUiTooling)
+    debugImplementation(Dependencies.composeUiTestManifest)
+}
