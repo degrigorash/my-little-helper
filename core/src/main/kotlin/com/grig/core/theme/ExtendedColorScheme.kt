57b9706613bffa869cbac4f59ae287fe3f1dcc14
ikozlenko <ikozlenko@toogoodtogo.com>
2026-03-12 15:51:41 +0100

    new main screen

---
+++ core/src/main/kotlin/com/grig/core/theme/ExtendedColorScheme.kt
@@ -0,0 +1,47 @@
+package com.grig.core.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+data class ExtendedColorScheme(
+    val gradientBackgroundTop: Color,
+    val gradientBackgroundBottom: Color,
+    val headerText: Color,
+    val cardText: Color,
+    val malCardStart: Color,
+    val malCardEnd: Color,
+    val malIconContainer: Color,
+    val danishCardStart: Color,
+    val danishCardEnd: Color,
+    val danishIconContainer: Color
+)
+
+val LightExtendedColorScheme = ExtendedColorScheme(
+    gradientBackgroundTop = gradientBackgroundTopLight,
+    gradientBackgroundBottom = gradientBackgroundBottomLight,
+    headerText = headerTextLight,
+    cardText = cardTextLight,
+    malCardStart = malCardStartLight,
+    malCardEnd = malCardEndLight,
+    malIconContainer = malIconContainerLight,
+    danishCardStart = danishCardStartLight,
+    danishCardEnd = danishCardEndLight,
+    danishIconContainer = danishIconContainerLight
+)
+
+val DarkExtendedColorScheme = ExtendedColorScheme(
+    gradientBackgroundTop = gradientBackgroundTopDark,
+    gradientBackgroundBottom = gradientBackgroundBottomDark,
+    headerText = headerTextDark,
+    cardText = cardTextDark,
+    malCardStart = malCardStartDark,
+    malCardEnd = malCardEndDark,
+    malIconContainer = malIconContainerDark,
+    danishCardStart = danishCardStartDark,
+    danishCardEnd = danishCardEndDark,
+    danishIconContainer = danishIconContainerDark
+)
+
+val LocalExtendedColorScheme = staticCompositionLocalOf { LightExtendedColorScheme }
