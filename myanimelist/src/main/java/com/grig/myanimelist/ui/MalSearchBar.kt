588370d2b9f3aa82281227e21a096dda964a32e5
ikozlenko <ikozlenko@toogoodtogo.com>
2026-03-17 13:15:10 +0100

    fis search field

--- myanimelist/src/main/java/com/grig/myanimelist/ui/MalSearchBar.kt
+++ myanimelist/src/main/java/com/grig/myanimelist/ui/MalSearchBar.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
@@ -65,7 +65,7 @@ fun MalSearchBar(
             onValueChange = onQueryChange,
             modifier = Modifier
                 .weight(1f)
-                .height(48.dp),
+                .heightIn(min = 48.dp),
             placeholder = {
                 Text(
                     text = placeholder,
